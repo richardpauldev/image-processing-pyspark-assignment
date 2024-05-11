3e6b3e77e4558a65e19ef1df3228a67e0047faf2
richardpauldev <richard@richardpauldev.com>
2024-05-11 13:22:30 -0400

    Jupyter Notebook Created

--- image-processing.py
+++ image-processing.py
@@ -1,10 +1,11 @@
 # Import necessary libraries
 from pyspark.ml.linalg import Vectors, VectorUDT
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col
-from pyspark.sql.types import BinaryType, DoubleType, ArrayType, IntegerType, FloatType, StructField, StructType
+from pyspark.sql.functions import udf
+from pyspark.sql.types import BinaryType, IntegerType, StructField, StructType
 import numpy as np
 from PIL import Image
+import pickle
 import io
 
 # Initialize a SparkSession
@@ -12,13 +13,13 @@ spark = SparkSession.builder \
     .appName("CIFAR-10 Image Processing with PySpark") \
     .config("spark.executor.memory", "4g") \
     .config("spark.driver.memory", "4g") \
+    .config("spark.memory.fraction", "0.6") \
     .config("spark.executor.memoryOverhead", "512m") \
-    .config("spark.sql.shuffle.partitions", "200") \
+    .config("spark.sql.shuffle.partitions", "100") \
     .getOrCreate()
 
 # Function to unpickle the CIFAR-10 dataset files
 def unpickle(file):
-    import pickle
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
     return dict
@@ -40,17 +41,12 @@ def load_cifar10_batch(file):
     images_and_labels = []
     
     for i in range(len(data)):
-        # Each row of data is an image
         image_array = data[i]
-        # Reshape the array into 3x32x32 and transpose to 32x32x3
         image_array_reshaped = image_array.reshape(3, 32, 32).transpose(1, 2, 0)
-        # Convert the image array to an image
         image = Image.fromarray(image_array_reshaped)
-        # Convert the image to bytes
         img_byte_arr = io.BytesIO()
         image.save(img_byte_arr, format='PNG')
         image_bytes = img_byte_arr.getvalue()
-        # Append the image bytes and label to the list
         images_and_labels.append((image_bytes, labels[i]))
     
     return images_and_labels
@@ -101,18 +97,14 @@ train_df, test_df = df.randomSplit([0.8, 0.2], seed=42)
 
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml import Pipeline
-
-rf = RandomForestClassifier(featuresCol="features", labelCol="label", numTrees=10)
-
-pipeline = Pipeline(stages=[rf])
-
-model = pipeline.fit(train_df)
-
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+
+rf = RandomForestClassifier(featuresCol="features", labelCol="label", numTrees=50, maxDepth=10)
+pipeline = Pipeline(stages=[rf])
+model = pipeline.fit(train_df)
 
 evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="accuracy")
 accuracy = evaluator.evaluate(model.transform(test_df))
 print(f"Accuracy: {accuracy}")
 
-# Stop the SparkSession
 spark.stop()
